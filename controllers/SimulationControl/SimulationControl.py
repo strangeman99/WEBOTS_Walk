b789165daa43cc3c7e722bf81a87b4f9406d651a
cdneeee <acdne@icloud.com>
2023-08-16 20:09:23 -0400

    Set boxes as a transform child for main robot, deleted the "Super" robot

--- controllers/SimulationControl/SimulationControl.py
+++ controllers/SimulationControl/SimulationControl.py
@@ -2,7 +2,7 @@ from math import sqrt
 from controller import Supervisor
 import random
 
-TIME_STEP = 32
+TIME_STEP = 16
 
 supervisor = Supervisor()
 
@@ -11,7 +11,7 @@ robot_node = supervisor.getFromDef("SUPER")
 trans_field = robot_node.getField("translation")
 rot_field = robot_node.getField("rotation")
 
-while supervisor.step(TIME_STEP) != -1:
+#while supervisor.step(TIME_STEP) != -1:
 
     #may     compute travelled distance
     #need   values = trans_field.getSFVec3f()
@@ -19,10 +19,10 @@ while supervisor.step(TIME_STEP) != -1:
             #print("a=%d, b=%d -> dist=%g" % (a, b, dist))
 
         # set the cubes position
-    rndx = random.uniform(-5, 5)
-    rndy = random.uniform(-5, 5)
-    POS = [rndx, rndy, 0]
-    trans_field.setSFVec3f(POS)
-    rot = random.uniform(0, 6.28319)
-    angle = [0, 0, 1, rot]
-    rot_field.setSFRotation(angle)
+rndx = random.uniform(-5, 5)
+rndy = random.uniform(-5, 5)
+POS = [rndx, rndy, 0]
+trans_field.setSFVec3f(POS)
+rot = random.uniform(0, 6.28319)
+angle = [0, 0, 1, rot]
+rot_field.setSFRotation(angle)
